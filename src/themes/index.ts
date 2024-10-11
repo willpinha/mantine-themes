8851a2a9425908188c7e7eb302f6f2db85173cbb
Willian Pinheiro <willpinhaaa@gmail.com>
2024-10-10 21:55:15 -0300

    add christmas theme

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,7 +1,8 @@
 import type { Theme } from "@/lib/themes";
+import { christmas } from "./chirstmas";
 import { dracula } from "./dracula";
 import { forest } from "./forest";
 
-const themes: Theme[] = [dracula, forest];
+const themes: Theme[] = [dracula, forest, christmas];
 
-export default themes;
+export default themes.sort((a, b) => (a.slug > b.slug ? 1 : -1));
