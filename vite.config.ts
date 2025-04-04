2420353bbbc18d54c567b3b2acb7fce1eb2fe874
Willian Pinheiro <willpinhaaa@gmail.com>
2025-04-04 15:06:22 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	base: "/",
+	base: "/mantine-themes",
 	plugins: [
 		reactRouter({
 			prerender: true,
